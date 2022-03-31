19d34d3d2f7f5f914e6ee11493beeecb62b9115d
ericderace <heberic@gmail.com>
2022-03-31 19:40:25 -0300

    a

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <SPI.h>
 #include <U8x8lib.h>
-#include <BluetoothSerial.h>
+#include "BluetoothSerial.h"
 
 #define PWMPIN 33
 #define PWMFREQ 5000
@@ -10,6 +10,7 @@
 
 // Structs, constants and variables
 U8X8_SSD1306_128X64_NONAME_SW_I2C u8x8(15, 4, 16);
+BluetoothSerial SerialBT;
 
 // Function prototypes
 
@@ -19,9 +20,17 @@ void setup_pwm();
 // Functions
 
 // sets up PWM
-void setup_pwm() {
+void setup_pwm()
+{
   ledcSetup(PWMCHANNEL, PWMFREQ, PWMRES);
   ledcAttachPin(PWMPIN, PWMCHANNEL);
+}
+
+// Set up OLED display
+void setup_oled()
+{
+  u8x8.begin();
+  u8x8.setFont(u8x8_font_chroma48medium8_r);
 }
 
 // Arduino default functions
@@ -30,23 +39,31 @@ void setup(void)
 {
   Serial.begin(115200);
   setup_oled();
+  if (SerialBT.begin("ESP32test")) Serial.println("Bluetooth started."); // Bluetooth device name
 }
 
 void loop(void)
 {
-
   static unsigned long timestamp = 0;
   unsigned long since = (millis() - timestamp);
 
   float fps = 1.0 / since * 1000.0;
-
   String fps_str = "FPS: " + String(fps);
+
+  if (Serial.available())
+  {
+    //SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available())
+  {
+    Serial.write(SerialBT.read());
+    //u8x8.drawString(0, 1, String(SerialBT.read()).c_str());
+  }
+
+  //static uint8_t duty = 255;
+  //ledcWrite(PWMCHANNEL, duty);
 
   u8x8.drawString(0, 2, fps_str.c_str());
   u8x8.drawString(0, 3, String(millis()).c_str());
-
-  static uint8_t duty = 255;
-  ledcWrite(PWMCHANNEL, duty);
-
   timestamp = millis();
 }
